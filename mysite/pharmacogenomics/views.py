f8ff614724b0910d3e9fef3601e42d013173b2b7
Henry Tan <henrytan508@gmail.com>
2020-01-11 09:39:37 -0700

    finishing up sider searcher

--- mysite/pharmacogenomics/views.py
+++ mysite/pharmacogenomics/views.py
@@ -1,10 +1,10 @@
+from django.db.models import Count
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views import generic
 from .models import SideEffect
 
 
-class IndexView(generic.ListView):
-    model = SideEffect
+class IndexView(generic.TemplateView):
     template_name = 'pharmacogenomics/index.html'
 
 
@@ -20,7 +20,6 @@ class SideEffectView(generic.ListView):
         if request.method == 'POST':
             self.side_effect_list = request.POST.getlist('sideEffectList[]')
             request.session['side_effect_list'] = self.side_effect_list
-            session_side_effect_list = request.session.get('side_effect_list')
             return HttpResponse('pharmacogenomics:side-effect-results', {'side_effect_list': self.side_effect_list})
 
 
@@ -31,6 +30,16 @@ class SideEffectResultsView(generic.ListView):
     def get_queryset(self):
         session_side_effect_list = self.request.session.get('side_effect_list')
         return self.model.objects.filter(side_effect__in=session_side_effect_list)
+
+
+class SideEffectRankedDrugsView(generic.ListView):
+    model = SideEffect
+    template_name = 'pharmacogenomics/side-effect-drugs-ranked.html'
+
+    def get_queryset(self):
+        session_side_effect_list = self.request.session.get('side_effect_list')
+        return self.model.objects.filter(side_effect__in=session_side_effect_list)\
+            .values('drug_name').annotate(dcount=Count('drug_name'))
 
 
 class ContactView(generic.ListView):
