18131587152efd7049c9743b36d9e2a02651360c
Henry Tan <henrytan508@gmail.com>
2020-03-14 23:19:38 -0600

    fixing db

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -84,7 +84,7 @@ BOOTSTRAP3 = {'include_jquery': True}
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
