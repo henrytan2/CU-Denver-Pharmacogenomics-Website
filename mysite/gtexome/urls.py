4e9e983bdabc8ed621f3bb1793d95b490f8ff27f
Henry Tan <henrytan508@gmail.com>
2020-03-22 19:38:24 -0600

    fixing csf

--- mysite/gtexome/urls.py
+++ mysite/gtexome/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -7,5 +6,5 @@ app_name = 'gtexome'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='gtexome'),
     path('results/', views.ResultsView.as_view(), name='results'),
-    path(r'exome/', csrf_exempt(views.ExomeView.as_view()), name='exome'),
+    path(r'exome/', views.ExomeView.as_view(), name='exome'),
 ]
