049aea94c4a9a551d009aa672e5483d22e22e25a
Henry Tan <henrytan508@gmail.com>
2020-12-12 15:30:48 -0700

    redoing virtual environment

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/henry/Documents/Pharmacogenomics Project/Pharmacogenomics website/pharmacogenomics_website/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
