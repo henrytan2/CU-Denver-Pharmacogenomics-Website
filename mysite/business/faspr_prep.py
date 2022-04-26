b50ebded33543118028ac8d3c94378c72aa799e5
scottmreed <53488644+scottmreed@users.noreply.github.com>
2022-04-25 21:07:00 -0600

    prepping FASPR inputs

--- mysite/business/faspr_prep.py
+++ mysite/business/faspr_prep.py
@@ -23,6 +23,7 @@ class FasprPrep:
     scratch_folder = os.path.join('/','storage','chemistry','projects','pharmacogenomics')
     alpha_folder = os.path.join(scratch_folder, 'alphafold')
     temp_folder = os.path.join(scratch_folder, 'tmp')
+    singularity_folder = os.path.join('..','..','..','storage','singularity')
 
     def __init__(self, CCID, gene_ID):
         self.alderaan = Alderaan()
@@ -30,18 +31,17 @@ class FasprPrep:
         self.gene_ID = gene_ID
         self.get_Pnum()
         self.unmutated_seq = self.get_sequence_unmut()
-        self.get_mut_pos = self.get_mutation_position(CCID)
+        self.mut_pos = self.get_mutation_position(CCID)
         self.get_mut_seq = self.get_mutated_sequence(self.unmutated_seq)
-        # self.client = alderaan.paramiko.client.SSHClient()
+        self.make_mutatedseq_file(self.mut_pos)
+        self.get_specific_mutation()
 
     def get_Pnum(self):
         with open('../pharmacogenomics_website/resources/ENSG_PN_dictALL.pickle', 'rb') as f:
             ENSG_Pnum_dict = pickle.load(f)
             self.P_num = ENSG_Pnum_dict[f'{self.gene_ID}']
-        print('P_num is',self.P_num)
 
     def get_sequence_unmut(self):
-        # return ('sampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstringsampleproteinstring')
         p = PDBParser()
         protein_count_command = f'ls -dq {self.alpha_folder}/AF-{self.P_num}-F*-model_v* | wc -l'
         protein_count, success = self.alderaan.run_command(protein_count_command)
@@ -51,12 +51,29 @@ class FasprPrep:
                 protein_filename = f"find '{self.alpha_folder}' -maxdepth 1 -name 'AF-{self.P_num}-F*-model_v*.pdb.gz'"
                 pdb_file, success = self.alderaan.run_command(protein_filename)
                 _ , protein_short_name = os.path.split(pdb_file[:-4])
-                self.alderaan.run_command(f'gunzip -k {pdb_file} > {self.temp_folder}/{protein_short_name}')
-                structure = p.get_structure('xx',f'{self.temp_folder}/{protein_short_name}')
+                mkdir_command = f'mkdir {self.temp_folder}/{protein_short_name[:-4]}'
+                try:
+                    self.alderaan.run_command(mkdir_command)
+                except:
+                    print('directory exists')
+                gunzip_command = f'gunzip -c {pdb_file[:-1]} > {self.temp_folder}/{protein_short_name[:-4]}/{protein_short_name}'
+                self.alderaan.run_command(gunzip_command)
+                open_command = f"cat {self.temp_folder}/{protein_short_name[:-4]}/{protein_short_name}"
+                outfile, success = self.alderaan.run_command(open_command)
+                print(success)
+                print(outfile)
+                f = open("pdb_tmp.txt", "wt+")
+                f.write(outfile)
+                print(f)
+                p = PDBParser(PERMISSIVE=1)
+                structure = p.get_structure(id = '_', file = 'pdb_tmp.txt')
+                f.close()
                 ppb = PPBuilder()
-                pp = ppb.build_peptides(structure)
-                PDBs = str(pp.get_sequence())
-                unmutated_sequence = PDBs.lower()
+                peptides = ppb.build_peptides(structure)
+                print(len(peptides))
+                PDB_sequence = peptides[0].get_sequence()
+                print(PDB_sequence)
+                unmutated_sequence = PDB_sequence.lower()
                 return unmutated_sequence
             else:
                 print('protein in multiple files. Skipped.')
@@ -73,34 +90,48 @@ class FasprPrep:
                 return mutation_position
 
     def get_mutated_sequence(self, seq):
-        position_mutation = self.get_mutation_position(self.CCID)
+        self.position_mutation = self.get_mutation_position(self.CCID)
+        print('position_mutation',self.position_mutation)
+        if self.position_mutation - self.neighbors < 1:
+            self.neighbors = self.position_mutation
+        possible_mutation_ext = str(self.CCID)
+        position_mutation = self.get_mutation_position(possible_mutation_ext)#drop?
+        self.sur_aa_low = self.unmutated_seq[position_mutation - self.neighbors:position_mutation - 1].upper()
+        print('sur_aa_low',self.sur_aa_low)
+        self.sur_aa_high = self.unmutated_seq[position_mutation:position_mutation + (self.neighbors-1)].upper()
+        print('sur_aa_high',self.sur_aa_high)
+        INV = possible_mutation_ext[2:5]
+        MNV = possible_mutation_ext[-3:]
+        self.single_nucleotide = SeqUtils.IUPACData.protein_letters_3to1[f'{INV}'].lower()
+        print(self.single_nucleotide)
+        self.single_nucleotide_variation = SeqUtils.IUPACData.protein_letters_3to1[f'{MNV}']
+        print(self.single_nucleotide_variation)
         self.mutated_protein_code = self.unmutated_seq[0:position_mutation - self.neighbors] + \
                                self.sur_aa_low + \
                                self.unmutated_seq[position_mutation - 1]\
-                                   .replace(self.single_nucleotide,
-                                            self.single_nucleotide_variation) + self.sur_aa_high + \
-                                            self.unmutated_seq[position_mutation + 6:]
+                                .replace(self.single_nucleotide,
+                                self.single_nucleotide_variation) + self.sur_aa_high + \
+                                self.unmutated_seq[position_mutation + (self.neighbors-1):]
+        print(self.mutated_protein_code)
         return self.mutated_protein_code
 
     def make_mutatedseq_file(self, mutatseq):
-        mutated_seq_file = open(f"repack_{self.P_num}_{self.CCID[2:]}.txt", "w")
+        mutated_seq_file = open(f"repack_{self.P_num}_{self.CCID[2:]}.txt", "w+")
+        mutated_seq_file.write(f"{self.mutated_protein_code}")
         mutated_seq_file.close()
-        return mutated_seq_file.write(f"{self.mutated_protein_code}")
 
     def get_specific_mutation(self):
-        poss_mutation_ext = self.CCID
-        possible_mutation_ext = str(poss_mutation_ext)
-        position_mutation = self.get_mutation_position(possible_mutation_ext)
 
-        INV = possible_mutation_ext[2:5]
-        MNV = possible_mutation_ext[-3:]
-        self.sur_aa_low = self.unmutated_seq[position_mutation - self.neighbors:position_mutation - 1].upper()
-        self.sur_aa_high = self.unmutated_seq[position_mutation:position_mutation + 6].upper()
-        self.single_nucleotide = SeqUtils.IUPACData.protein_letters_3to1[f'{INV}'].lower()
-        self.single_nucleotide_variation = SeqUtils.IUPACData.protein_letters_3to1[f'{MNV}']
-        if self.unmutated_seq[position_mutation - 1] == f'{self.single_nucleotide}':
+        if self.unmutated_seq[self.position_mutation - 1] == f'{self.single_nucleotide}':
             mutated_protein_code = self.get_mutated_sequence(self.unmutated_seq)
             self.make_mutatedseq_file(mutated_protein_code)
-        self.alderaan.run_command(f"singularity shell /storage/singularity/qvina.sif")
-        self.alderaan.run_command(
-            f"/FASPR/FASPR -i /{self.scratch_folder}{self.unzip_target_pdb_file} -s /{self.scratch_folder}/repack_lines/repack_{self.P_num}_{possible_mutation_ext[2:]}.txt  -o /{self.scratch_folder}/proteinmutations/{self.P_num}_{self.possible_mutation_ext}_FASPR.pdb")
+
+        pwd, success = self.alderaan.run_command(f"pwd")
+        print(success)
+        print(pwd)
+        singularity, success = self.alderaan.run_command(f"ls {self.singularity_folder}")
+        print (success)
+        singularity, success = self.alderaan.run_command(f"singularity shell {self.singularity_folder}/qvina.sif")
+        print (success)
+        # self.alderaan.run_command(
+        #     f"/FASPR/FASPR -i /{self.scratch_folder}{self.unzip_target_pdb_file} -s /{self.scratch_folder}/repack_lines/repack_{self.P_num}_{possible_mutation_ext[2:]}.txt  -o /{self.scratch_folder}/proteinmutations/{self.P_num}_{self.possible_mutation_ext}_FASPR.pdb")
