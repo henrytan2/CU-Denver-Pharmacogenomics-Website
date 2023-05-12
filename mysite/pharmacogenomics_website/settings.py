19d42cfd463249549f8ace9750c348927af377b5
scottmreed <53488644+scottmreed@users.noreply.github.com>
2023-05-11 19:13:26 -0600

    trying again to help django find static css files by fixing collectstatic error

--- mysite/pharmacogenomics_website/settings.py
+++ mysite/pharmacogenomics_website/settings.py
@@ -255,9 +255,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 
-    'django_plotly_dash.finders.DashAssetFinder',
-    'django_plotly_dash.finders.DashComponentFinder',
-    'django_plotly_dash.finders.DashAppDirectoryFinder',
+    # 'django_plotly_dash.finders.DashAssetFinder',
+    # 'django_plotly_dash.finders.DashComponentFinder',
+    # 'django_plotly_dash.finders.DashAppDirectoryFinder',
 ]
 
 PLOTLY_COMPONENTS = [
