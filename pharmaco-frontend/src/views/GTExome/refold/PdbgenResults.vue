84d9c69276bec07f38bd19edfed9150e8d97985f
jessica2tan <jessica.2.tan@ucdenver.edu>
2024-10-06 17:08:44 -0600

    adding base api url to pdb gen results

--- pharmaco-frontend/src/views/GTExome/refold/PdbgenResults.vue
+++ pharmaco-frontend/src/views/GTExome/refold/PdbgenResults.vue
@@ -2,12 +2,13 @@
 import { usePdbgenStore } from '@/stores/PdbgenStore'
 
 const pdbgenStore = usePdbgenStore()
+const apiBaseUrl = import.meta.env.VITE_API_BASE_URL
 </script>
 <template>
   <iframe
     class="d-flex border-0 w-100"
     height="800px"
     id="pdbgen-results"
-    :src="`http://localhost:8000/pdbgen/pdbgen-results/?session_key=${pdbgenStore.storePdbGenDataResponse.session_key}`"
+    :src="`${apiBaseUrl}/pdbgen/pdbgen-results/?session_key=${pdbgenStore.storePdbGenDataResponse.session_key}`"
   ></iframe>
 </template>
